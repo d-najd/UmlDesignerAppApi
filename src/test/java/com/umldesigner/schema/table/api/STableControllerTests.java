013b8d949ca339f88a36ae799f85ea77e66ef888
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-07-24 16:50:31 +0200

    added more controller tests, switched float comparasons to hamcest

--- src/test/java/com/umldesigner/schema/table/api/STableControllerTests.java
+++ src/test/java/com/umldesigner/schema/table/api/STableControllerTests.java
@@ -5,10 +5,13 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -75,8 +78,8 @@ public class STableControllerTests {
                     // checking for size n stuff will be useless if there are no items in the table
                     .andExpect(jsonPath("$.items", is(not(empty()))))
                     .andExpect(jsonPath("$.items", hasSize(mock.getItems().size())))
-                    .andExpect(jsonPath("$.x").value(mock.getX()))
-                    .andExpect(jsonPath("$.y").value(mock.getY()));
+                    .andExpect(jsonPath("$.x", is(closeTo(mock.getX(), 0.000001))))
+                    .andExpect(jsonPath("$.y", is(closeTo(mock.getY(), 0.000001))));
         } catch (Exception e) {
             e.printStackTrace();
             fail();
@@ -105,8 +108,8 @@ public class STableControllerTests {
                     // checking for size n stuff will be useless if there are no items in the table
                     .andExpect(jsonPath("$[0].items", is(not(empty()))))
                     .andExpect(jsonPath("$[0].items", hasSize(mockSTableListPojo.get(0).getItems().size())))
-                    .andExpect(jsonPath("$[0].x").value(mockSTableListPojo.get(0).getX()))
-                    .andExpect(jsonPath("$[0].y").value(mockSTableListPojo.get(0).getY()))
+                    .andExpect(jsonPath("$[0].x", is(closeTo(mockSTableListPojo.get(0).getX(), 0.000001))))
+                    .andExpect(jsonPath("$[0].y", is(closeTo(mockSTableListPojo.get(0).getY(), 0.000001))))
                     .andExpect(jsonPath("$[1].title", is(mockSTableListPojo.get(1).getTitle())));
         } catch (Exception e) {
             e.printStackTrace();
@@ -116,32 +119,63 @@ public class STableControllerTests {
         verify(this.sTableService).getAll();
     }
 
-    /*
-     * @Test
-     * public void updateBookTest_success() {
-     * String mockUuid = "mockUUID";
-     * STablePojo mock = STableTestUtil.createMockSTablePojo("mock title 1",
-     * "mock description 1");
-     * STablePojo updatedMock = STableTestUtil.createMockSTablePojo("mock title 1",
-     * "mock description 1");
-     * 
-     * when(this.sTableService.updateBook(mockUuid, mock)).thenReturn(updatedMock);
-     * 
-     * try {
-     * String jsonBodyPayload = objectMapper.writer().writeValueAsString(mock);
-     * 
-     * this.mockMvc.perform(put(Endpoints.BOOKS + mockUuid)
-     * .contentType(MediaType.APPLICATION_JSON)
-     * .content(jsonBodyPayload))
-     * .andDo(print())
-     * .andExpect(status().is2xxSuccessful())
-     * .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-     * .andExpect(jsonPath("$.title").value(updatedMock.getTitle()))
-     * .andExpect(jsonPath("$.description").value(updatedMock.getDescription()));
-     * } catch (Exception e) {
-     * e.printStackTrace();
-     * fail();
-     * }
-     * }
-     */
+    @Test
+    @DisplayName("Create Schema Table")
+    public void createSTable() {
+        STablePojo mock = STableTestUtil.createMockTablePojo();
+        STablePojo createdMock = STableTestUtil.createMockTablePojo();
+
+        when(this.sTableService.createSchemaTable(mock)).thenReturn(createdMock);
+
+        try {
+            String jsonBodyPayload = objectMapper.writer().writeValueAsString(mock);
+
+            this.mockMvc.perform(post(Endpoints.TABLE)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonBodyPayload))
+                    .andDo(print())
+                    .andExpect(status().is2xxSuccessful())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.title", is(mock.getTitle())))
+                    // checking for size n stuff will be useless if there are no items in the table
+                    .andExpect(jsonPath("$.items", is(not(empty()))))
+                    .andExpect(jsonPath("$.items", hasSize(mock.getItems().size())))
+                    .andExpect(jsonPath("$.x", is(closeTo(mock.getX(), 0.000001))))
+                    .andExpect(jsonPath("$.y", is(closeTo(mock.getY(), 0.000001))));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    @DisplayName("Update Schema Table")
+    public void updateSTable() {
+        String mockUuid = "mockUUID";
+        STablePojo mock = STableTestUtil.createMockTablePojo();
+        STablePojo updatedMock = STableTestUtil.createMockTablePojo();
+
+        when(this.sTableService.updateSchemaTable(mockUuid, mock)).thenReturn(updatedMock);
+
+        try {
+            String jsonBodyPayload = objectMapper.writer().writeValueAsString(mock);
+
+            this.mockMvc.perform(put(Endpoints.TABLE + "/" + mockUuid)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonBodyPayload))
+                    .andDo(print())
+                    .andExpect(status().is2xxSuccessful())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.title", is(mock.getTitle())))
+                    // checking for size n stuff will be useless if there are no items in the table
+                    .andExpect(jsonPath("$.items", is(not(empty()))))
+                    .andExpect(jsonPath("$.items", hasSize(mock.getItems().size())))
+                    .andExpect(jsonPath("$.x").value(mock.getX()))
+                    .andExpect(jsonPath("$.y").value(mock.getY()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
 }
